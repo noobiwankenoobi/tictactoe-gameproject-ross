1578335dc6953498455e349d1fb587aac32dbcfb
noobiwankenoobi <ross.hazelett@gmail.com>
2017-04-13 11:32:03 -0400

    Created folder to house gameApi actions and userApi actions
    Created and nested new files int the scripts/game folder
    Created gameTemplate, gameChecks, gameMoves
    Put some starter code into the gameApi/game-ui file
    
    4/13 11:33am

--- assets/scripts/game/engine.js
+++ assets/scripts/game/engine.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const getFormFields = require(`../../lib/get-form-fields`)
+const getFormFields = require(`../../../lib/get-form-fields`)
 
-const api = require('./game-api')
-const ui = require('./game-ui.js')
+const api = require('../gameAPI/game-api')
+const ui = require('../gameAPI/game-ui.js')
 
 
 
